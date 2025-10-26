2c86a34514c5377e889ec9a75d9924d78505b666
SkutM <ssm011403@gmail.com>
2025-10-25 19:16:14 -0700

    deployment ready

--- api/main.py
+++ api/main.py
@@ -62,12 +62,13 @@ def _create_tables():
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
+    "https://reading-tracker-qahrn94mu-scotts-projects-69acb861.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
